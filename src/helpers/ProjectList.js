dcb68b32e8d33e7cb5d1b8169669af1071d6a876
Atif Mehrban <p180024@nu.edu.pk>
2022-10-04 14:57:24 +0500

    initial commit

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.jpg";
+import Proj3 from "../assets/proj3.jpg";
+import Proj4 from "../assets/proj4.webp";
+import Proj5 from "../assets/proj5.jpg";
+import Proj6 from "../assets/proj6.jpg";
+
+export const ProjectList = [
+  {
+    name: "Tour Advisor",
+    image: Proj1,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Ecommerce Website",
+    image: Proj2,
+    skills: "React,Node.js,MongoDB",
+  },
+  {
+    name: "Counter App",
+    image: Proj3,
+    skills: "React,Node.js,MongoDB,SpotifyAPI",
+  },
+  {
+    name: "Social Media Website",
+    image: Proj4,
+    skills: "React,Node.js,MySQL,GraphQL",
+  },
+  {
+    name: "Amazon",
+    image: Proj5,
+    skills: "EC2,RDS,clone",
+  },
+  {
+    name: "Mobile Game",
+    image: Proj6,
+    skills: "React Native,JavaScript,HTML,CSS",
+  },
+];
